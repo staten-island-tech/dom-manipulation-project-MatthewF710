2e632d7571920aa221ead30f35aadac7b1e9ae6c
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-10-09 13:59:04 -0400

    fdsFVsVCsz

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// create HTML for inputs, cards and container aka where the cards go
+//select/query the HTML form and get values from inputs
+//turn values into an object
+//insert card with object onto HTML
+//add event listener for remove button in JS
 const DOMSelectors = {
   //finding stuff
   button: document.querySelector(".btn"),
   form: document.querySelector(".form"),
+  container: document.querySelector(".container"),
 };
 let currentcard;
 let currentdesc;
@@ -20,5 +26,9 @@ DOMSelectors.form.addEventListener("submit", function (event) {
     currentdesc,
     "\nImage (link):",
     currentimg
+  );
+  DOMSelectors.container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card"><h1>${currentcard}</h1><img src="${currentimg}" alt="card-img" class="card-img"><p>${currentdesc}</p> </div>`
   );
 });
